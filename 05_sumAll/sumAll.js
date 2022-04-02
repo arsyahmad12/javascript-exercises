9308f40ed425203660dfd39907e4ac7b67b6f58b
arsyahmad12 <arsyahmad14@gmail.com>
2022-04-02 16:45:24 +0700

    Finished Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(startNum, endNum) {
+    sum = 0;
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR"
+    } else if (startNum >= 0 && endNum >= 0) {
+        if (startNum <= endNum) {
+            for (let i = startNum; i <= endNum; i++) {
+                sum += i
+            }
+        } else if (endNum < startNum) {
+            for (let i = endNum; i <= startNum; i++) {
+                sum += i
+            }
+        }
+    } else {
+        return "ERROR"
+    }
+    return sum
 };
-
 // Do not edit below this line
 module.exports = sumAll;
