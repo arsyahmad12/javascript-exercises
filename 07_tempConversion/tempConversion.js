812e649e12e36227e878696d7e0e2d793b754839
arsyahmad12 <arsyahmad14@gmail.com>
2022-04-02 18:25:30 +0700

    Finished Exercise 7, Project 1

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(suhu) {
+  convSuhu = (5/9) * (suhu - 32);
+  return Math.round(convSuhu * 10) / 10
 };
 
-const ctof = function() {
-
+const ctof = function(suhu) {
+  convSuhu = (9/5)*suhu + 32;
+  return Math.round(convSuhu * 10) / 10
 };
 
 // Do not edit below this line
