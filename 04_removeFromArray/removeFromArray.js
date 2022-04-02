88e18a0eda07bf3ca16cde0e729e312d073c5e37
arsyahmad12 <arsyahmad14@gmail.com>
2022-04-02 14:29:22 +0700

    Finished Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(list, arg1, arg2, arg3, arg4) {
+    for (let i = 1; i < arguments.length; i++) {
+        let temp = list.indexOf(arguments[i]);
+        if (temp >= 0) {
+            list.splice(temp, 1);
+        }
+    }
+    return list;
 };
 
 // Do not edit below this line
