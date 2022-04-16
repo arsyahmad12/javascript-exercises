d410ca681c0f107d37c79fec5d575e5078c4d08c
arsyahmad12 <arsyahmad14@gmail.com>
2022-04-17 01:52:00 +0700

    Finished Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+fibonacciNumbers = [1, 1]
 
+const fibonacci = function(num) {
+    for (let i = 2; i < 100000; i++) {
+        fibonacciNumbers[i] = fibonacciNumbers[i-1] + fibonacciNumbers[i-2]
+    }
+    if (num < 1) return "OOPS"
+    return fibonacciNumbers[num-1]
 };
 
 // Do not edit below this line
