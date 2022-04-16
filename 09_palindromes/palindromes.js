f3a81a0b1b540bc5cc9bd913e9f19167a09bd1a8
arsyahmad12 <arsyahmad14@gmail.com>
2022-04-17 01:23:35 +0700

    Finished Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    newString = string.replace(/[^\w]/g, "").toLowerCase()
+    return (newString === reverseString(newString))
 };
 
+const reverseString = function(str) {
+    reversedString = ""
+    for (let i = 1; i <= str.length; i++) {
+        reversedString += str[str.length - i]
+    }
+    return reversedString
+};
+
+console.log(palindromes("Racecar!"))
 // Do not edit below this line
 module.exports = palindromes;
